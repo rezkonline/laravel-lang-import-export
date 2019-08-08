768223a32ac2d79a462f6fd6bc128ef6799aa83b
Aidas Klimas <aidaskk@gmail.com>
2019-08-08 16:49:04 +0300

    comments

--- src/config/lang_import_export.php
+++ src/config/lang_import_export.php
@@ -1,8 +1,11 @@
 <?php
 return [
+    /**
+     * In practice export_locale value is 'en' and export_target is a list of all locales supported by your website
+     */
     'base_locale' => 'en',
     'export_locale' => 'en',// locale list separated by comma
-    'export_target' => null,
+    'export_target' => null,// target locale list separated by comma
     'base_group' => null, // all groups or comma separated list
     'export_path' => storage_path(':locale:target.csv'),
 ];
